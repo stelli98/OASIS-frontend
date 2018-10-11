d5ffc166ed23415e518cdb96de19c81cf3583a7a
stelli98_ <stellitan98@gmail.com>
2018-10-11 22:56:09 +0700

    V.1.1  Seperate the code into seperated file (component)

--- Oasis/assets/js/base.js
+++ Oasis/assets/js/base.js
@@ -1,3 +1,43 @@
 $(document).ready(function(){
-  	$('.sidebar').load("../components/sidebar/sidebar.html");
+	
+	$(".sidebar__part").load("../components/sidebar/sidebar.html",function(){
+			$(".navbar__part").load("../components/navbar/navbar.html",function(){
+				$(".status__part").load("../components/dashboardStatus/dashboardStatus.html",function(){
+					 $(".new-request__part").load("../components/dashboardNewAssetRequest/dashboardNewAssetRequest.html");
+				});
+			});
+	  });
+
+    //  LOGIN BUTTON
+	  $('#btn-login').click(function(){
+		var username = $('#username').val();
+		var password = $('#password').val();
+
+		var userdata = {
+			  "username":username,
+			  "password":password
+		  }
+
+		$.ajax({
+		  type: 'POST',
+		  url: 'http://localhost:8085/oasis/login',
+		  contentType: 'application/json',
+		  data: JSON.stringify(userdata),
+		  dataType: 'json',
+		  success: function(data){
+			  if(data.code == 200){
+				  alert(data.value.employeeId);
+				  window.location.href = "../../views/dashboard.html"
+			  } else {
+				 	  $(".login__error").css("display","block");
+					  $(".login__error-message").text(data.value.errorMessage);
+				 	  $( ".form__input").addClass( "form__input-error");
+			  }
+		  },
+		  error: function(data){
+			  alert("failed");
+		  },
+		  processData: false
+	  });
+	});
 });
