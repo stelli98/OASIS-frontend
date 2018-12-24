9ab5fdb2b7d99fcceb4aaef61fe65b318eafc826
stelli98_ <stellitan98@gmail.com>
2018-12-24 21:58:35 +0700

    OASIS V 2.3 Employee & Request DONE

--- Oasis/assets/js/base.js
+++
@@ -1,9 +0,0 @@
-export const statusSuccess=200;
-export const path='http://localhost:8085/oasis';
-
-export const elements={
-    btnAssetTableEdit:$(".table-content-asset-admin-edit"),
-    btnAssetListAdd:$(".btn-asset-list-add"),
-    btnAssetListDelete:$(".btn-asset-list-delete")
-
-}
